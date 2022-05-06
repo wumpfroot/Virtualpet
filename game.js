9d7cb720e8cc41b95180edd75d83b64acea41605
wumpfroot <markuskojo@msn.com>
2022-05-06 17:02:12 +0100

    Second commit

--- game.js
+++ game.js
@@ -13,47 +13,56 @@ const objectLvlWalks = document.getElementById("objectLvlWalks");
 const petBoxSingular = document.getElementById("petBoxSingular");
 const buttonRevivePet = document.getElementById("buttonRevivePet");
 
+// Audio
+var audio = new Audio("cartoon-kitty-begging-meow.wav");
+audio.volume = 0.07; //Changes the volume of the MEOW
+
+
 //petIcon//
 document.getElementById("petIcon").src="cyberPetCatNeutral.png";
 
 
 //Interacting with Pet//
 buttonPlayWith.addEventListener("click", ()=>{ //increases value of play level when clicked//
+    audio.play();
+    audio.currentTime = 0;
     objectLvlPlay.value+=20;
-})
-buttonPlayWith.addEventListener("mousedown", ()=>{ //makes cat happy when interacted with//
     petIcon.src="cyberPetCatHappy.png";
+    setTimeout(() => {
+        petIcon.src="cyberPetCatNeutral.png";    
+    }, 2500);
 })
-buttonPlayWith.addEventListener("mouseup", ()=>{ //returns cat to neutral state on mouse release//
-    petIcon.src="cyberPetCatNeutral.png";
-})
+
 buttonFeed.addEventListener("click", ()=>{ //increases value of hunger level when clicked//
+    audio.play();
+    audio.currentTime = 0;
     objectLvlHunger.value+=20;
-})
-buttonFeed.addEventListener("mousedown", ()=>{
     petIcon.src="cyberPetCatHappy.png";
+    setTimeout(() => {
+        petIcon.src="cyberPetCatNeutral.png";    
+    }, 2500);
 })
-buttonFeed.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
-})
+
 buttonWater.addEventListener("click", ()=>{//increases value of thirst level when clicked//
+    audio.play();
+    audio.currentTime = 0;
     objectLvlThirst.value+=20;
-})
-buttonWater.addEventListener("mousedown", ()=>{
     petIcon.src="cyberPetCatHappy.png";
+    setTimeout(() => {
+        petIcon.src="cyberPetCatNeutral.png";    
+    }, 2500);
 })
-buttonWater.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
-})
+
 buttonWalk.addEventListener("click", ()=>{//increases value of walks level when clicked//
+    audio.play();
+    audio.currentTime = 0;
     objectLvlWalks.value+=20;
-})
-buttonWalk.addEventListener("mousedown", ()=>{
     petIcon.src="cyberPetCatHappy.png";
+    setTimeout(() => {
+        petIcon.src="cyberPetCatNeutral.png";    
+    }, 2500);
 })
-buttonWalk.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
-})
+
 buttonKillPet.addEventListener("click", () => { //to make pet dead//
     objectLvlPlay.value=0;
     objectLvlHunger.value=0;
@@ -65,12 +74,14 @@ buttonKillPet.addEventListener("click", () => { //to make pet dead//
     buttonRevivePet.style.display = "block";
 })
 buttonRevivePet.addEventListener("click", () => { //to revive cat//
+    audio.play();
     objectLvlPlay.value=1000;
     objectLvlHunger.value=100;
     objectLvlThirst.value=100;
     objectLvlWalks.value=100;
     petIcon.src="cyberPetCatNeutral.png";
-    petBoxSingular.style.background = " #F4CEC7";
+    petBoxSingular.style.background = "#d49d49";
+    // petBoxSingular.style.background = " #F4CEC7";
     buttonKillPet.style.display = "block";
     buttonRevivePet.style.display = "none";
 })
